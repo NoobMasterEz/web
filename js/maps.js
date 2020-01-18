ee07f1469371a6f4fa0f6aff51c07f77f08e6728
ratchanonth <b13m123kiss@Gmail.com>
2020-01-18 12:53:17 +0700

    test

---
+++ js/maps.js
@@ -0,0 +1,55 @@
+//Data
+var cities = [
+   
+    {
+        city : 'thailand  ',
+        desc : 'Kathipara Bridge!',
+        lat : 18.80968,
+        long : 98.9506823
+    }
+];
+
+//Angular App Module and Controller
+var sampleApp = angular.module('mapsApp', []);
+sampleApp.controller('MapCtrl', function ($scope) {
+
+    var mapOptions = {
+        zoom: 15,
+        center: new google.maps.LatLng(18.80968,98.9506823),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $scope.markers = [];
+    
+    var infoWindow = new google.maps.InfoWindow();
+    
+    var createMarker = function (info){
+        
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.city
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+        
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open($scope.map, marker);
+        });
+        
+        $scope.markers.push(marker);
+        
+    }  
+    
+    for (i = 0; i < cities.length; i++){
+        createMarker(cities[i]);
+    }
+
+    $scope.openInfoWindow = function(e, selectedMarker){
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
+
+});
